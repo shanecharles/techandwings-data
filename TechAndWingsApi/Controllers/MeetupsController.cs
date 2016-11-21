e01ac9b7ef331af4007465396b0617c1c7b0cca3
shane charles <shanecharles@burningicesolutions.com>
2016-11-21 17:03:24 -0600

    convert the json file to an object in order to convert it to json?

--- TechAndWingsApi/Controllers/MeetupsController.cs
+++ TechAndWingsApi/Controllers/MeetupsController.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Hosting;
+using Newtonsoft.Json;
 
 namespace TechAndWingsApi.Controllers
 {
@@ -17,14 +18,13 @@ namespace TechAndWingsApi.Controllers
     /// Retrieves the current season's meetups for Tech and Wings Brandon.
     /// </summary>
     /// <returns>Json only</returns>
-    public async Task<HttpResponseMessage> Get()
+    public async Task<object> Get()
     {
       using (var stream = new StreamReader(File.OpenRead(HostingEnvironment.MapPath("~/app_data/meetups.json"))))
       {
         var meetupData = await stream.ReadToEndAsync();
-        var response = Request.CreateResponse(HttpStatusCode.OK);
-        response.Content = new StringContent(meetupData.Replace("\r\n",""), Encoding.UTF8, "application/json");
-        return response;
+        var data = JsonConvert.DeserializeObject(meetupData);
+        return data;
       }
     }
   }
